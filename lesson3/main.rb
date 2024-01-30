cb7134dddceb8b0c6be6555a575e3934cef818db
slydevruby <sly.dev@zohomail.com>
2024-01-30 21:11:33 +0700

    Almost done

--- lesson3/main.rb
+++ lesson3/main.rb
@@ -13,18 +13,31 @@ def create_station
   $stations << station
 end
 
-def stations_list
-  index = 0
-  puts "Список станций:"
-  $stations.each do |station|
-    puts "#{index} Станция #{station.name}"
-    index += 1
+def show_stations
+  puts "Всего станций: #{$stations.size}"
+  $stations.each_with_index do |station, index|
+    puts "  #{index}  Станция #{station.name}"
   end
-  index
+end
+
+def show_routes
+  puts "Всего маршрутов: #{$routes.size}"
+  $routes.each_with_index do |route, index|
+    print "  #{index} Маршрут сообщением"
+    puts " #{route.stations.first.name} #{route.stations.last.name}"
+  end
+end
+
+def show_trains
+  puts "Всего поездов: #{$trains.size}"
+  $trains.each_with_index do |train, index|
+    puts "  #{index} Поезд #{train.name}, #{train.type}, вагонов: #{train.amount}"
+  end
 end
 
 def create_route
-  index = stations_list
+  show_stations
+  index = $stations.size
   
   first = 0
   second = 1
@@ -45,65 +58,98 @@ def create_route
   $routes << Route.new($stations[first], $stations[second])
 end
 
+def input_loop(title, top)
+  number = 0
+  loop do
+    puts "Выберите #{title} 0..#{top} по номеру"
+    number = gets.chomp.to_i
+    break if (0..top).include? number
+    puts "Такого номера нет. Повторим"
+  end
+  number
+end
+
+def assign_route
+  show_trains
+  train_no = input_loop('поезд', $trains.size - 1)
+  show_routes
+  route_no = input_loop('маршрут', $routes.size - 1) 
+  $trains[train_no].assign_route($routes[route_no])
+end
+
+
+
 def add_station_to_route
-  puts "add_station_to_route"
+  show_routes
+  route_number = input_loop('маршрут', $routes.size - 1)
+  show_stations
+  station_number = input_loop('станцию', $stations.size - 1)
+  puts 'Добавляем станцию к маршруту'
+  $routes[route_number].add_station($stations[station_number])  
 end
 
 def rem_station_from_route
   puts "rem_station_from_route"
 end
 
-
-
 def create_train
   puts "Введите название"
   name = gets.chomp
   train = Train.new(name, :cargo, 10)
   $trains << train
-
-  puts $trains
 end
 
 def trains_on_station
-  puts 'list of train on station!'
+  show_stations
+  no = input_loop('станцию', $stations.size - 1)
+  puts "На станции #{$stations[no].name} поездов: #{$stations[no].trains.size}"
+  $stations[no].trains.each { |tr|  puts "Train #{tr.name}"  }
 end
 
-def trains_list
-  index = 0
-  $trains.each do |train|
-    puts "#{index} Поезд #{train.name}, #{train.type}, вагонов: #{train.amount}"
-    index += 1
-  end
+def show_stations_on_route
+  show_routes
+  route_number = input_loop('маршрут', $routes.size - 1)
+  $routes[route_number].stations.each { |station| puts "  Станция #{station.name}" }
 end
 
 def chain_wagon
-  trains_list
+  show_trains
 end
-
 
 def unchain_wagon
+  show_trains
 end
 
-def routes_list
-  $routes.each do |route|
-    print 'Маршрут сообщением '
-    puts "#{route.stations.first.name} #{route.stations.last.name}"
-  end
+def forward
+  show_trains
+  train_no = input_loop('поезд', $trains.size - 1)
+  $trains[train_no].forward
+end
+
+def backward
+  show_trains
+  train_no = input_loop('поезд', $trains.size - 1)
+  $trains[train_no].backward
 end
 
 $menu = {Станция: [], Маршрут: [], Поезд: []}
 $menu[:Станция] << {title: 'Создать станцию', meth: :create_station}
 $menu[:Станция] << {title: 'Показать список поездов на станции', meth: :trains_on_station}
+$menu[:Станция] << {title: 'Показать список станций', meth: :show_stations}
 
 $menu[:Маршрут] << {title: 'Создать Маршрут', meth: :create_route}
-$menu[:Маршрут] << {title: 'Список Маршрутов', meth: :routes_list}
 $menu[:Маршрут] << {title: 'Добавить станцию к маршруту', meth: :add_station_to_route}
 $menu[:Маршрут] << {title: 'Убрать станцию с маршрута', meth: :rem_station_from_route}
+$menu[:Маршрут] << {title: 'Показать список станций на маршруте', meth: :show_stations_on_route}
+$menu[:Маршрут] << {title: 'Список Маршрутов', meth: :show_routes}
 
 $menu[:Поезд] << {title: 'Создать', meth: :create_train}
-$menu[:Поезд] << {title: 'Показать список', meth: :trains_list}
+$menu[:Поезд] << {title: 'Присвоить маршрут', meth: :assign_route}
 $menu[:Поезд] << {title: 'Добавить вагоны', meth: :chain_wagon}
 $menu[:Поезд] << {title: 'Отцепить вагоны', meth: :unchain_wagon}
+$menu[:Поезд] << {title: 'Вперед по маршруту', meth: :forward}
+$menu[:Поезд] << {title: 'Назад по маршруту', meth: :backward}
+$menu[:Поезд] << {title: 'Показать список', meth: :show_trains}
 
 
 #puts menu
@@ -124,7 +170,7 @@ def show_menu
   index
 end
 #puts $menu
-show_menu
+#show_menu
 
 loop do
   index = show_menu
